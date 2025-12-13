ccf1c13308fb6e321955319dae3e6e24a7f6ef18
dachi <ani10sanadze@gmail.com>
2025-12-13 04:07:30 +0400

    build base of layouts, add routing and page components, plus header and footer component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './features/home/pages/home-page/home-page';
+import { LoginPage } from './features/login/pages/login-page/login-page';
+import { RegisterPage } from './features/register/pages/register-page/register-page';
+import { ProfilePage } from './features/profile/pages/profile-page/profile-page';
 
 export const routes: Routes = [
 
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomePage },
+    { path: 'login', component: LoginPage },
+    { path: 'register', component: RegisterPage }, // !CanDeactive guard will be added later!
+    { path: 'profile/:username', component: ProfilePage }, // !CanActive + CanDeactive will be added later!
 
-    
+
+    { path: '**', redirectTo: 'home' } // always keep last!
 ];
+

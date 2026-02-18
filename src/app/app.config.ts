3fe334bc14072efb2b9d284729ed75fff8e06f33
dachi <ani10sanadze@gmail.com>
2026-02-18 10:07:29 +0400

    added lazy loading on background, directly from main branch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -9,7 +9,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes, withInMemoryScrolling({
+    provideRouter(routes,
+      withPreloading(PreloadAllModules), // preload lazy-loaded routes but in background
+      withInMemoryScrolling({
       scrollPositionRestoration: 'enabled', // renders new page from top: 0
     })),
     provideHttpClient(),
